c1457cc7cac5fc7a2beecdec50ba67d6f865faf7
David Geraldo <davidgrldo@Davids-MacBook-Pro.local>
2022-02-12 17:54:53 +0700

    first commit

---
+++ src/main/java/sanbox/basicspring/HelloWorldConfiguration.java
@@ -0,0 +1,8 @@
+package sanbox.basicspring;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+    //
+}
---
+++ src/test/java/sanbox/basicspring/ApplicationContextTest.java
@@ -0,0 +1,12 @@
+package sanbox.basicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+    @Test
+    void testApplicationContext() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+    }
+}
